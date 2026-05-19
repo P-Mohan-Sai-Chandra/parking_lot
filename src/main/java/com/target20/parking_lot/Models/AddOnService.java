dd21612663517ab1362fd5669c8c1a6dad198435
P-Mohan-Sai-Chandra <pulukuri.mohansai@gmail.com>
2026-05-20 00:30:01 +0530

    Add repository stubs and enum mapping
    Add empty repository classes for AddOnServiceOrder, AddOnService, Bill, Operator, ParkingSlot, ParkingSpot, Payment, and Vehile in com.target20.parking_lot.Repositories. Update AddOnService model to annotate serviceName with @Enumerated(EnumType.STRING) and add related imports so enum values are persisted as strings. These repository stubs provide placeholders for future data access implementations.

--- src/main/java/com/target20/parking_lot/Models/AddOnService.java
+++ src/main/java/com/target20/parking_lot/Models/AddOnService.java
@@ -1,6 +1,9 @@
 package com.target20.parking_lot.Models;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,6 +14,7 @@ import lombok.ToString;
 @ToString
 public class AddOnService extends  BaseModel {
 
+    @Enumerated(value = EnumType.STRING)
     private AddOnServiceName serviceName;
     private double servicePrice;
     private ServiceAvailability serviceStatus;
